4b4db600f33fa45074cf1c879e86a589ab7907bb
Luiz Fernando de Frias <l.frias@globo.com>
2010-12-22 15:49:38 -0200

    Consertado um erro de sintaxe

--- media/js/action.js
+++ media/js/action.js
@@ -992,7 +992,7 @@ $(document).ready(function(){
 $('#motivoVindaUnidadeSaude').change(function(){
 	if ($(this).val() == 'tosse'){
 		$('#tosse').val('sim');
-		#('#tosse').change();
+		$('#tosse').change();
 	}
 });
 /*------------------------------------------------------------------------------------------------*/
