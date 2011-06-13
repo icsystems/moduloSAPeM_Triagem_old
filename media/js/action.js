fe9cb545bb4dca77788476281d52cbf4f0975191
Andressa Sivolella <andressasivolella@gmail.com>
2011-06-13 14:58:49 -0300

    Retirando a obrigatoriedade do campo 'Tempo de emagrecimento' em 	'Sinais, sintomas e dados clinicos';

--- media/js/action.js
+++ media/js/action.js
@@ -318,6 +318,24 @@ $(document).ready(function(){
 				$(dep[div]).toggle();
 		}
 	}
+	$.fn.showTempoEmagrecimentoField = function(argumento){
+		var dep = argumento;
+		for(div in dep){
+			var elems = $('*', dep[div]);
+			$(elems).each(function(){
+				var element = $(this);
+				if (   element[0].nodeName != 'FIELDSET'
+					&& element[0].nodeName != 'SMALL'
+					&& element[0].nodeName != 'OPTION')
+					$(this).removeAttr('disabled',false);
+				});
+			if($(dep[div]).css('display') != 'block')
+				$(dep[div]).toggle(function() {
+					$(this).css('background-color', hlcolor);
+					$(this).animate({backgroundColor : "white"}, 4000);
+					});
+		}
+	}
 
 /* --------------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------------*/
@@ -759,6 +777,7 @@ $(document).ready(function(){
 			else
 				$().hideFields(dep);
 	});
+
 /*------------------------------------------------------------------------------------------------*/
 /*---------------------------------- Logica  do Emagrecimento ------------------------------------*/
 	// Check emagrecimento field
@@ -769,7 +788,7 @@ $(document).ready(function(){
 		var valorPeso = parseInt($('#pesoHabitual').val(),10);
 		if ((valor != 0)&&(valorPeso != 0))
 			if (valor < valorPeso)
-				$().showFields(dep);
+				$().showTempoEmagrecimentoField(dep);
 			else{
 				$().hideFields(dep);
 				$('#emagrecimento').val('Não');
@@ -782,7 +801,7 @@ $(document).ready(function(){
 		var valorPeso = parseInt($('#pesoAtual').val(),10);
 		if ((valor != 0)&&(valorPeso != 0))
 			if (valorPeso < valor)
-				$().showFields(dep);
+				$().showTempoEmagrecimentoField(dep);
 			else{
 				$().hideFields(dep);
 				$('#emagrecimento').val('Não');
