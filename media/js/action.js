88b8b754bf6f15d44c40e768b377ec7f682c4272
Andressa Sivolella <andressasivolella@gmail.com>
2011-10-26 22:52:08 -0200

    Alterando campo "Tempo de Emagrecimento" de meses para semanas.

--- media/js/action.js
+++ media/js/action.js
@@ -814,8 +814,9 @@ $(document).ready(function(){
 				$('#emagrecimento').val('Não');
 			}
 	});
+
 	$('#pesoAtual').change(function(){
-		var tempoEmagrecimento = parseInt($('#tempoEmagrecimento').val(),10);
+		var tempoEmagrecimento = parseInt($('#tempoEmagrecimentoSemanas').val(),10) / 4;
 		var percentagem = (parseInt($('#pesoHabitual').val(),10) - parseInt($('#pesoAtual').val(),10))/parseInt($('#pesoHabitual').val(),10);
 		if (tempoEmagrecimento >= 1 && tempoEmagrecimento < 3)
 			if(percentagem > 0.05)
@@ -833,8 +834,9 @@ $(document).ready(function(){
 			else
 				$('#emagrecimento').val('Não');
 	});
+
 	$('#pesoHabitual').change(function(){
-		var tempoEmagrecimento = parseInt($('#tempoEmagrecimento').val(),10);
+		var tempoEmagrecimento = parseInt($('#tempoEmagrecimentoSemanas').val(),10) / 4;
 		var percentagem = (parseInt($('#pesoHabitual').val(),10) - parseInt($('#pesoAtual').val(),10))/parseInt($('#pesoHabitual').val(),10);
 		if (tempoEmagrecimento >= 1 && tempoEmagrecimento < 3)
 			if(percentagem > 0.05)
@@ -852,8 +854,9 @@ $(document).ready(function(){
 			else
 				$('#emagrecimento').val('Não');
 	});
-	$('#tempoEmagrecimento').change(function(){
-		var tempoEmagrecimento = parseInt($('#tempoEmagrecimento').val(),10);
+
+	$('#tempoEmagrecimentoSemanas').change(function(){
+		var tempoEmagrecimento = parseInt($('#tempoEmagrecimentoSemanas').val(),10) / 4;
 		var percentagem = (parseInt($('#pesoHabitual').val(),10) - parseInt($('#pesoAtual').val(),10))/parseInt($('#pesoHabitual').val(),10);
 		if (tempoEmagrecimento >= 1 && tempoEmagrecimento < 3)
 			if(percentagem > 0.05)
@@ -871,6 +874,8 @@ $(document).ready(function(){
 			else
 				$('#emagrecimento').val('Não');
 	});
+
+
 /*------------------------------------------------------------------------------------------------*/
 /*------------------------------------ Logica da Tosse -------------------------------------------*/
 $('#motivoVindaUnidadeSaude').change(function(){
