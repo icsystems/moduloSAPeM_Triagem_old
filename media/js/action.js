349b40225cb3ec6116cd1bef9d374608bdbebcc6
Andressa Sivolella <andressasivolella@gmail.com>
2011-11-25 14:47:18 -0200

    Permitindo inserção de número decimal para perguntas "Peso atual" e 	"Peso habitual";

--- media/js/action.js
+++ media/js/action.js
@@ -395,6 +395,11 @@ $(document).ready(function(){
 			return false;
 		}
 	});
+	$('.floatNumber').keypress(function(e){
+		if ((e.which > 31 && e.which < 44 && e.which > 44 && e.which < 48) || (e.which > 57)){
+			return false;
+		}
+	});
 
 	$('.hour').livequery('keypress', function(e){
 		if((e.which > 31 && e.which < 48)||(e.which > 57))
@@ -783,8 +788,8 @@ $(document).ready(function(){
 	$('#pesoAtual').change(function(){
 		var dep = new Array();
 		dep[0] = '#divTempoEmagrecimento';
-		var valor = parseInt($(this).val(),10);
-		var valorPeso = parseInt($('#pesoHabitual').val(),10);
+			var valor = parseFloat($(this).val(),10);
+		var valorPeso = parseFloat($('#pesoHabitual').val(),10);
 		if ((valor != 0)&&(valorPeso != 0))
 			if (valor < valorPeso)
 				$().showTempoEmagrecimentoField(dep);
@@ -796,8 +801,8 @@ $(document).ready(function(){
 	$('#pesoHabitual').change(function(){
 		var dep = new Array();
 		dep[0] = '#divTempoEmagrecimento';
-		var valor = parseInt($(this).val(),10);
-		var valorPeso = parseInt($('#pesoAtual').val(),10);
+		var valor = parseFloat($(this).val(),10);
+		var valorPeso = parseFloat($('#pesoAtual').val(),10);
 		if ((valor != 0)&&(valorPeso != 0))
 			if (valorPeso < valor)
 				$().showTempoEmagrecimentoField(dep);
@@ -809,7 +814,7 @@ $(document).ready(function(){
 
 	$('#pesoAtual').change(function(){
 		var tempoEmagrecimento = parseInt($('#tempoEmagrecimentoSemanas').val(),10) / 4;
-		var percentagem = (parseInt($('#pesoHabitual').val(),10) - parseInt($('#pesoAtual').val(),10))/parseInt($('#pesoHabitual').val(),10);
+		var percentagem = (parseFloat($('#pesoHabitual').val(),10) - parseFloat($('#pesoAtual').val(),10))/parseFloat($('#pesoHabitual').val(),10);
 		if (tempoEmagrecimento >= 1 && tempoEmagrecimento < 3)
 			if(percentagem > 0.05)
 				$('#emagrecimento').val('Sim');
@@ -829,7 +834,7 @@ $(document).ready(function(){
 
 	$('#pesoHabitual').change(function(){
 		var tempoEmagrecimento = parseInt($('#tempoEmagrecimentoSemanas').val(),10) / 4;
-		var percentagem = (parseInt($('#pesoHabitual').val(),10) - parseInt($('#pesoAtual').val(),10))/parseInt($('#pesoHabitual').val(),10);
+		var percentagem = (parseFloat($('#pesoHabitual').val(),10) - parseFloat($('#pesoAtual').val(),10))/parseFloat($('#pesoHabitual').val(),10);
 		if (tempoEmagrecimento >= 1 && tempoEmagrecimento < 3)
 			if(percentagem > 0.05)
 				$('#emagrecimento').val('Sim');
@@ -849,7 +854,7 @@ $(document).ready(function(){
 
 	$('#tempoEmagrecimentoSemanas').change(function(){
 		var tempoEmagrecimento = parseInt($('#tempoEmagrecimentoSemanas').val(),10) / 4;
-		var percentagem = (parseInt($('#pesoHabitual').val(),10) - parseInt($('#pesoAtual').val(),10))/parseInt($('#pesoHabitual').val(),10);
+		var percentagem = (parseFloat($('#pesoHabitual').val(),10) - parseFloat($('#pesoAtual').val(),10))/parseFloat($('#pesoHabitual').val(),10);
 		if (tempoEmagrecimento >= 1 && tempoEmagrecimento < 3)
 			if(percentagem > 0.05)
 				$('#emagrecimento').val('Sim');
